6030d545eff4ccca4ffca302d1243e2a311baa64
nambudi2 <nambudi2@illinois.edu>
2016-11-02 16:55:24 -0500

    Added in 1.6 and 1.7

--- mini_project/main.py
+++ mini_project/main.py
@@ -5,11 +5,22 @@ from numpy.random import choice
 
 nucleotides = ["A", "T", "C", "G"]
 
-def create_binding_sites(sequence_count, motif_matrix):
+def output_plant_sites(plant_sites):
+    """ Outputs the `plant_sites` array to sites.txt, one site per line """ 
+    with open("sites.txt", "w+") as output_file:
+        output_file.write("\n".join(str(i) for i in plant_sites))
+
+def output_sequences(sequences):
+    """ Prints the `sequences` out to "sequences.fa", in FASTA Format """
+    with open("sequences.fa", "w+") as output_file:
+        output_file.write(">" + "\n>".join(sequences))
+
+def create_binding_sites(sequence_count, motif_matrix, length):
+    """ Create `sequence_count` `binding-sites` of length `length`, by sampling the `motif-matrix` """
     binding_sites = []
     for i in range(sequence_count):
         seq = []
-        for j in range(len(motif_matrix[0])):
+        for j in range(length):
              seq.append(choice(nucleotides, p=[motif_matrix[k][j] for k in range(len(motif_matrix))]))
         binding_sites.append("".join(seq))
     return binding_sites
@@ -29,10 +40,12 @@ def create_random_motif(icpc, length):
 def plant_site(seqs, sites):
     """ Over-rides a substring from each sequence in `seqs` with a seq from `sites` """
     new_seqs = []
+    plant_sites = []
     for i in range(len(seqs)):
-        idx = random.randint(0, len(seq) - len(site) - 1)
-        new_seqs.append(seq[:idx] + site + seq[idx+1:])
-    return new_seqs
+        idx = random.randint(0, len(seqs[i]) - len(sites[i]) - 1)
+        new_seqs.append(seqs[i][:idx] + sites[i] + seqs[i][idx+len(sites[i]):])
+        plant_sites.append(idx)
+    return new_seqs, plant_sites
 
 def create_sequences(count, length):
     """ Creates `count` sequences of `length` each. Returns an array of length `count`"""
@@ -54,11 +67,19 @@ def main():
 
     sequences = create_sequences(sc, sl)
     motif = create_random_motif(icpc, ml)
-    binding_sites = create_binding_sites(sc, motif)
-    new_seqs = plant_site(sequences, binding_sites)
+    binding_sites = create_binding_sites(sc, motif, ml)
+    new_seqs, plant_sites = plant_site(sequences, binding_sites)
 
-    print (sequences)
-    print (new_seqs)
+    print(sequences)
+    print(binding_sites)
+    print(new_seqs)
+    print(plant_sites)
+
+    output_sequences(new_seqs)
+    output_plant_sites(plant_sites)
+
+
+
 
 if __name__ == "__main__":
     main()
