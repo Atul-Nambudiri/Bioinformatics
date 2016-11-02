432570534b7e43d3b714a18bcce881683e20a9fc
Allen Kleiner <akleiner24@gmail.com>
2016-11-02 15:53:21 -0500

    Step 1.3 complete (generate random motif)

--- mini_project/main.py
+++ mini_project/main.py
@@ -3,6 +3,15 @@ import sys
 
 nucleotides = ["A", "T", "C", "G"]
 
+def create_random_motif(icpc, length):
+    """ Creates a 4 x `length` matrix, where the sum of each column of the matrix is `icpc` """
+    motif = [[0 for cols in range(length)] for rows in range(4)]
+    for i in range(4):
+        for j in range(length):
+            motif[i][j] = random.uniform(0, (icpc - sum(row[i] for row in motif)))
+
+    return motif
+
 def create_sequences(count, length):
     """ Creates `count` sequences of `length` each. Returns an array of length `count`"""
     seqs = []
@@ -22,6 +31,7 @@ def main():
     sc = int(sys.argv[4])
 
     sequences = create_sequences(sc, sl)
-
+    motif = create_random_motif(icpc, ml)
+    
 if __name__ == "__main__":
     main()
